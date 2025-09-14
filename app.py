c510c0f2563bab6d2c6258c8c0e0727e41d97051
gharshitha516 <gharshitha516@gmail.com>
2025-09-14 12:53:36 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 # Load correction model
 @st.cache_resource
 def load_model():
-    model_name = "harshhitha/FTe2_Misspelling"
+    model_name = "harshhitha/FTe1_Misspelling_Correction"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     return model, tokenizer
